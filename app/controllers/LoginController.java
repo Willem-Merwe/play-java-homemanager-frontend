7895ce6bfcbc00a2aff23b0a0f3c2ddb5aa1f46c
Willem-Merwe Joubert <merwejoubert@gmail.com>
2018-06-21 23:45:09 +0200

    Initial Upload

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -9,13 +9,7 @@ import views.html.*;
  * to the application's home page.
  */
 public class LoginController extends Controller {
-
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    
     public Result result() {
         return ok(views.html.login.render("Home Management"));
     }
